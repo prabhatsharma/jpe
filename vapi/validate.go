57918b5698383f34a590971116bf6e3a01091ba5
Prabhat Sharma <hi.prabhat@gmail.com>
2021-06-14 16:14:57 -0700

    cleaned up code

--- vapi/validate.go
+++ vapi/validate.go
@@ -1,26 +1,14 @@
 package vapi
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/dop251/goja"
 	"github.com/gin-gonic/gin"
 	v1 "k8s.io/api/admission/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Validate(c *gin.Context) {
-	vm := goja.New()
-	v, err := vm.RunString("2 + 2")
-	if err != nil {
-		panic(err)
-	}
-	if num := v.Export().(int64); num != 4 {
-		panic(num)
-	} else {
-		fmt.Println(v.Export().(int64))
-	}
 
 	var aReviewRequest v1.AdmissionReview
 	c.BindJSON(&aReviewRequest)
@@ -38,9 +26,10 @@ func Validate(c *gin.Context) {
 		Message: "Enjoy your life",
 	}
 
+	// This is the response that is returned to k8s
 	aReviewResponse := v1.AdmissionResponse{
 		UID:     aReviewRequest.Request.UID,
-		Allowed: true,
+		Allowed: true, // Set this to true for when validation succeeds. Set this to false for failed validation
 		Result:  status,
 	}
 

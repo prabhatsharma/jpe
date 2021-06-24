7e6b5cdb4314793aaa27ddfd2a1e980fe7e67f89
Prabhat Sharma <hi.prabhat@gmail.com>
2021-06-24 07:58:02 -0700

    logging

--- api/v1alpha1/validate_resource.go
+++ api/v1alpha1/validate_resource.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	"fmt"
+
 	v1 "k8s.io/api/admission/v1"
 )
 
@@ -13,6 +15,7 @@ func ValidateResource(aReviewRequest *v1.AdmissionReview) RuleResponse {
 			rr = ValidateRule(&rule, aReviewRequest)
 			if !rr.Allowed {
 				rr.Message = "Policy/Rule: " + admissionPolicy.Name + "/" + rr.Message
+				fmt.Println(rr.Message)
 				return rr // return immediately if any rule fails
 			}
 		}

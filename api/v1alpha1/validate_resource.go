820bf8eb16132d282e984b6b925240958ddb745e
Prabhat Sharma <hi.prabhat@gmail.com>
2021-06-24 05:56:44 -0700

    removed more logging

--- api/v1alpha1/validate_resource.go
+++ api/v1alpha1/validate_resource.go
@@ -11,7 +11,7 @@ func ValidateResource(aReviewRequest *v1.AdmissionReview) RuleResponse {
 
 	for _, admissionPolicy := range AdmissionPolicies {
 		for _, rule := range admissionPolicy.Spec.Rules {
-			fmt.Println(rule.Rule)
+			// fmt.Println(rule.Rule)
 			rr = ValidateRule(&rule, aReviewRequest)
 			if !rr.Allowed {
 				rr.Message = admissionPolicy.Name + " : " + rr.Message

cd97faa094db735a046db5076bbf6c7cf50171ed
Prabhat Sharma <hi.prabhat@gmail.com>
2021-06-24 06:31:52 -0700

    removed request log

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ var (
 )
 
 func main() {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Recovery())
+	// r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
